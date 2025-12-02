ead156ef2f49eca7c1c49d976959a16085ee66f9
lynn <okolo.ebelelynda@gmail.com>
2025-12-02 13:06:28 +0100

    updated hardhat.config.ts to include celo

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -35,11 +35,11 @@ export default defineConfig({
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
     celo: {
-     type: "http",
-     chainType: "l1",
-     url: process.env.CELO_MAINNET_RPC_URL!,
-     accounts: [process.env.PRIVATE_KEY_DEPLOYER!],
-     chainId: 42220,
+      type: "http",
+      chainType: "l1",
+      url: configVariable("CELO_MAINNET_RPC_URL"),
+      accounts: [configVariable("CELO_MAINNET_PRIVATE_KEY")],
+      chainId: 42220,
     },
   },
 });
